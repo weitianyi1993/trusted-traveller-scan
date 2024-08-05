ab38548eaba068677a32297b4c53f1e23b0fd467
weitianyi321 <weitianyi321@gmail.com>
2024-08-05 08:22:15 -0700

    sound support

--- scan.py
+++ scan.py
@@ -1,8 +1,10 @@
 import requests
 import time
 import smtplib
+import threading
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+import pygame
 
 # Constants
 URL = "https://ttp.cbp.dhs.gov/schedulerapi/slot-availability?locationId=5020"
@@ -30,7 +32,7 @@ def send_email(msg, sender_email, app_password, receiver_email):
         server.sendmail(sender_email, receiver_email, msg.as_string())
         server.quit()
         print("Email sent successfully!")
-        time.sleep(CHECK_INTERVAL)
+        time.sleep(SEND_INTERVAL)
     except Exception as e:
         print(f"Failed to send email: {e}")
 
@@ -46,13 +48,37 @@ def check_slot_availability(url):
         print(f"An error occurred: {e}")
         return None
 
+def play_sound():
+    # Initialize the mixer
+    pygame.mixer.init()
+    # Load the MP3 file
+    pygame.mixer.music.load("/Users/tianyiwei/Downloads/chime-sound-7143.mp3")
+    # Play the MP3 file
+    pygame.mixer.music.play()
+    # Wait for the music to finish playing
+    while pygame.mixer.music.get_busy():
+        pygame.time.Clock().tick(10)
+
 def main():
     """Main function to check slot availability and send email if available."""
     while True:
         data = check_slot_availability(URL)
         if data and data.get("availableSlots"):
+
+            # Create threads for each method
+            thread1 = threading.Thread(target=send_email)
+            thread2 = threading.Thread(target=play_sound)
+
+            # Start the threads
+            thread1.start()
+            thread2.start()
+
+            # Wait for both threads to complete
+            thread1.join()
+            thread2.join()
+
             subject = "Nexus appointment found"
-            body = "Please check!" 
+            body = "Please check!"
             email_msg = create_email(subject, body, SENDER_EMAIL, RECEIVER_EMAIL)
             send_email(email_msg, SENDER_EMAIL, APP_PASSWORD, RECEIVER_EMAIL)
         time.sleep(CHECK_INTERVAL)
